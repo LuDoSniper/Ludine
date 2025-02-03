d9b0c844bf83a756be9acf6662b82bf0b550db0b
LuDoSniper <lucien.donnarieix@hesias.fr>
2025-02-03 19:59:58 +0100

    Feat[BCK-02]: Gestion des containers (CRUD)

---
+++ src/Controller/Food/Stock/ContainerController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Controller\Food\Stock;
+
+use App\Entity\Food\Stock\Container;
+use App\Form\Food\Stock\ContainerType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ContainerController extends AbstractController
+{
+    public function __construct(
+        private readonly EntityManagerInterface $entityManager
+    ){}
+
+    #[Route('/food/stock/containers', 'food_stock_containers')]
+    public function containers(): Response
+    {
+        $containers = $this->entityManager->getRepository(Container::class)->findAll();
+
+        return $this->render('Page/Food/Stock/containers.html.twig', [
+            'containers' => $containers
+        ]);
+    }
+
+    #[Route('/food/stock/containers/create', 'food_stock_containers_create')]
+    public function create(
+        Request $request
+    ): Response
+    {
+        $container = new Container();
+
+        $form = $this->createForm(ContainerType::class, $container);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->entityManager->persist($container);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('food_stock_containers');
+        }
+
+        return $this->render('Page/Food/Stock/container-create.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    #[Route('/food/stock/containers/update/{id}', 'food_stock_containers_update')]
+    public function update(
+        Container $container,
+        Request $request
+    ): Response
+    {
+        $form = $this->createForm(ContainerType::class, $container);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('food_stock_containers');
+        }
+
+        return $this->render('Page/Food/Stock/container-update.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    #[Route('/food/stock/containers/remove/{id}', 'food_stock_containers_remove')]
+    public function remove(
+        Container $container
+    ): Response
+    {
+        $this->entityManager->remove($container);
+        $this->entityManager->flush();
+
+        return $this->redirectToRoute('food_stock_containers');
+    }
+}
